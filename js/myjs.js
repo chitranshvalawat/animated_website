1eacc85a134ffca8598e6a9ca462dc00219a88eb
Your Name <you@example.com>
2022-02-12 21:05:50 +0530

    updated

--- js/myjs.js
+++ js/myjs.js
@@ -1,13 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('.play-pause-btn').on('click',function(){
-        $('.play-pause-btn').addClass('animate');
-    setTimeout(function() {
-        $('.play-pause-btn').attr('data-click', 1)
-        $('.play-pause-btn').addClass('hide').removeClass('animate');
-        $('#video')[0].play();
-        $('.video').addClass('show');
-    }, 2000);
+    $('.play-pause-btn').on('click', function () {
+        $(this).addClass('animate');
+        let that = this
+        console.log(that.classList.contains("play-pause-btn4"));
+        setTimeout(function () {
+            $(that).removeClass('animate');
+            // console.log($(".play-pause-btn1").attr("data-click"))
+            // console.log($(".play-pause-btn2").attr("data-click"))
+            // console.log($(".play-pause-btn3").attr("data-click"))
+            // console.log(that.classList.contains(".play-pause-btn4") )
+            if (parseInt($(".play-pause-btn1").attr("data-click")) && parseInt($(".play-pause-btn2").attr("data-click")) && parseInt($(".play-pause-btn3").attr("data-click"))) {
+                $('.play-pause-btn').addClass('grow');
+                setTimeout(() => {
+                    $('#video')[0].play();
+                    $('.video').addClass('show');
+
+                }, 2000);
+
+
+            }
+            $(that).attr('data-click', 1)
+            // $('#video')[0].play();
+            // $('.video').addClass('show');
+        }, 2000);
     });
-    
-    });
+
+});
